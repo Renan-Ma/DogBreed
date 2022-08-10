824f2c661e50a8fe3423bbb1009ed82c0d2e253e
Renan Martinez <remam_martinez89@hotmail.com>
2022-08-09 21:00:22 -0300

    criado router

---
+++ src/Router/Router.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Routes } from "react-router-dom";
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route index element={<Register />}/>
+      <Route path="/list" element={<List />}/>
+      <Route path="*" element={<Error />}/>
+    </Routes>
+  )
+}
+
+export default Router
