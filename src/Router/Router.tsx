e76751b01fa962307a15e516fbd535ef56cff215
Renan Martinez <remam_martinez89@hotmail.com>
2022-08-09 21:03:16 -0300

    atualizando projeto

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Route, Routes } from "react-router-dom";
+// import React from 'react'
+// import { Route, Routes } from "react-router-dom";
 
-const Router = () => {
-  return (
-    <Routes>
-      <Route index element={<Register />}/>
-      <Route path="/list" element={<List />}/>
-      <Route path="*" element={<Error />}/>
-    </Routes>
-  )
-}
+// const Router = () => {
+//   return (
+//     <Routes>
+//       <Route index element={<Register />}/>
+//       <Route path="/list" element={<List />}/>
+//       <Route path="*" element={<Error />}/>
+//     </Routes>
+//   )
+// }
 
-export default Router
+// export default Router
