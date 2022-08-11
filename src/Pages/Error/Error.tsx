f7f171a6e264182e4f01389c513aca737b350d3f
Renan Martinez <remam_martinez89@hotmail.com>
2022-08-11 10:12:24 -0300

    criado pagina de erro

---
+++ src/Pages/Error/Error.tsx
@@ -0,0 +1,11 @@
+import { MainContainer } from './styled'
+
+const Error = () => {
+  return (
+    <MainContainer>
+      <h1>PÁGINA NÃO ENCONTRADA</h1>
+      </MainContainer>
+  )
+}
+
+export default Error
